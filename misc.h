a93a4c778d0d25d11110364dbbcbdc3d41810d8d
Lew <lew@wiedz.net.pl>
2022-09-16 20:36:02 +0200

    word by word input

--- misc.h
+++ misc.h
@@ -14,7 +14,12 @@
 
 #define CTRL_D 0x04
 #define CTRL_X 0x18
+#define IN_BACKSPACE 0x7f
 #define ERRNO_SIGCAUGHT 0x04
+#define UP_ARROW 0x00415b1b
+#define DOWN_ARROW 0x00425b1b
+#define RIGHT_ARROW 0x00435b1b
+#define LEFT_ARROW 0x00445b1b
 
 typedef unsigned int uint;
 
--- prompt.c
+++ prompt.c
@@ -57,67 +57,124 @@ int pty_readkey() {
 
 char buff[BUFF_SIZE+1];
 int buff_pos = 0;
-int line_start = 0;
 
-char *readl(int fd) {
+#define RRS_BACKSPACE 0x01
+#define RRS_EOL 0x02
+#define RRS_EOF 0x04
+#define RRS_OVERFLOW 0x08
+
+typedef struct {
+    char *text;
+    int  status;
+} ReadResult;
+
+ReadResult read_word(int fd, char *override_arg) {
     int n;
-    int old_line = line_start;
-    while((n = read(fd, buff+buff_pos, BUFF_SIZE-buff_pos)) > 0) {
-        write(STDOUT_FILENO, buff+buff_pos, n);
+    int line_start = buff_pos;
+    if(override_arg != NULL) {
+        line_start = override_arg - buff;
+    }
 
-        int end = -1;
-        for(int i = 0; i < n; i++) {
-            if(buff[buff_pos+i] == '\n') {
-                end = i;
-                break;
+    char sbuff;
+    while(BUFF_SIZE - buff_pos >= 8
+          && (n = read(fd, &sbuff, 1)) > 0) {
+
+        if(sbuff == CTRL_D)
+            return (ReadResult) { .status = RRS_EOF };
+
+        if(sbuff == 0x1b) { // ESC
+            char cmd;
+            if(read(fd, &cmd, 1) == 1) { 
+                if(cmd == 0x5b) { // Move Keys
+                    read(fd, &cmd, 1);
+                } 
             }
         }
-
-        buff_pos += n;
-        if(end >= 0) {
-            if(end == n-1) {
-                line_start = buff_pos = 0;
+        else if(sbuff == IN_BACKSPACE) {
+            if(buff_pos > line_start) {
+                write(STDOUT_FILENO, "\b", 1);
+                buff_pos--;
+            } else {
+                return (ReadResult) { .status = RRS_BACKSPACE };
             }
-            break;
+        }
+        else if(sbuff == ' ' || sbuff == '\t') {
+            if(buff_pos > line_start) {
+                write(STDOUT_FILENO, " ", 1);
+                buff[buff_pos++] = 0;
+                return (ReadResult) { .status = 0,
+                                      .text = buff + line_start };
+            }
+        }
+        else if(sbuff == '\n') {
+            write(STDOUT_FILENO, "\n", 1);
+            buff[buff_pos++] = 0;
+            return (ReadResult) { .status = RRS_EOL,
+                                  .text = buff + line_start };
+        } else {
+            write(STDOUT_FILENO, &sbuff, 1);
+            buff[buff_pos++] = sbuff;
         }
     }
 
-    return buff + old_line;
+    return (ReadResult) { .status = RRS_OVERFLOW,
+                          .text = buff + line_start };
 }
 
-struct termios ito, oto, eto;
+char *args[MAX_ARGS+1];
+char ** read_args(int fd) {
+    int n = 0;
+    buff_pos = 0;
+    while(n < MAX_ARGS) {
+        ReadResult ans = read_word(fd, NULL);
+        if(ans.status == RRS_OVERFLOW) {
+            fprintf(stderr, "\nOverflow, aborting…\n");
+            args[0] = NULL;
+            return args;
+        }
+
+        if(ans.status == RRS_EOF)
+            return NULL;
+
+        while(ans.status == RRS_BACKSPACE) {
+            if(n > 0) {
+                n--;
+                buff_pos--;
+                write(STDOUT_FILENO, "\b", 1);
+                ans = read_word(fd, args[n]);
+            } 
+        }
+
+        args[n++] = ans.text;
+        
+        if(ans.status & RRS_EOL)
+            break;
+    }
+
+    args[n] = NULL;
+    return args;
+}
+
+struct termios ito, oto, eto, termopt;
 void prompt(const CHLine *handlers, size_t handlers_cnt) {
     PTY ptys[MAX_PTYS];
     for(size_t i = 0; i < MAX_PTYS; i++)
         ptys[i] = NO_PTY;
 
-    char *args[MAX_ARGS];
-    char *icur;
+    char **argv;
 
     tcgetattr(STDIN_FILENO, &ito);
     tcgetattr(STDOUT_FILENO, &oto);
     tcgetattr(STDERR_FILENO, &eto);
 
-    struct termios termopt = ito;
+    termopt = ito;
     termopt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &termopt);
 
-    while(write(STDOUT_FILENO, "> ", 2), icur = readl(STDIN_FILENO)) {
-        char **ocur = args;
-        while(isspace(*icur)) icur++;
-        while(*icur != 0) {
-            *ocur = icur;
-            while(*icur > 0 && !isspace(*icur)) icur++;
-            if(*icur > 0) {
-                *icur = 0;
-                icur++;
-                while(isspace(*icur)) icur++;
-            }
-            ocur++;
-        }
-
-        *ocur = NULL;
-        if(*args == NULL) continue;
+    while(reset_tty(),
+          write(STDOUT_FILENO, "> ", 2),
+          argv = read_args(STDIN_FILENO)) {
+        if(*argv == NULL) continue;
 
         CommandHandler ch = NULL;
         for(size_t i = 0; i < handlers_cnt; i++) {
@@ -134,11 +191,11 @@ void prompt(const CHLine *handlers, size_t handlers_cnt) {
         }
     }
 
-    reset_tty();
-}
-
-void reset_tty() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &ito);
     tcsetattr(STDOUT_FILENO, TCSAFLUSH, &oto);
     tcsetattr(STDERR_FILENO, TCSAFLUSH, &eto);
+}
+
+void reset_tty() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &termopt);
 }
